0599d438a01042991ba6d803607411d02f9e872c
Robert Lipka <roberto.lipka@gmail.com>
2017-09-28 16:13:53 +0200

    sgasd

---
+++ PullRequest1/PoundsToKilos.java
@@ -0,0 +1,19 @@
+/* 
+Lecture 18, 
+Section 4
+Pounds To Kilos Converter
+*/
+
+public class PoundsToKilos {
+	
+public static void main(String[]args) {
+
+        double pounds = 160;
+        double kilos = pounds*(0.45359237);
+
+        System.out.println(pounds + "  pounds are " + kilos + " kilos.");
+
+    }
+
+	
+}
